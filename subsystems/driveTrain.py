504c455c0209d8f024baf2182497592d45a3e1ee
jserra99 <progamer5051@gmail.com>
2024-01-22 19:36:24 -0800

    added drivetrain periodic

--- subsystems/driveTrain.py
+++ subsystems/driveTrain.py
@@ -5,3 +5,17 @@ class DriveTrainSubsystem(commands2.Subsystem):
     def __init__(self, config: dict) -> None:
         super().__init__()
         self.config = config # inhereting the robot config json from the robot container
+        
+    def periodic(self) -> None:
+        if self.llTable.getNumber('getpipe', 0) == 0: # 0 being our apriltag pipeline
+            if self.llTable.getNumber('tv', 0) == 1: # are there any valid targets
+                if self.alliance == wpilib.DriverStation.Alliance.kBlue:
+                    botPoseData = self.llTable.getNumberArray('botpose_wpiblue', [0,0,0,0,0,0,0])
+                else:
+                    botPoseData = self.llTable.getNumberArray('botpose_wpired', [0,0,0,0,0,0,0])
+                botPose2D = geometry.Pose2d(geometry.Translation2d(botPoseData[0], botPoseData[1]), geometry.Rotation2d(botPoseData[5]))
+                latency = botPoseData[6]
+                self.poseEstimator.addVisionMeasurement(botPose2D, latency)
+        self.poseEstimator.update(
+            self.getNAVXRotation2d(),
+            self.getSwerveModulePositions())
--- subsystems/swerveModule.py
+++ subsystems/swerveModule.py
@@ -1,7 +1,7 @@
 import rev
+from phoenix5 import _ctre as ctre
 from wpimath import kinematics, geometry
 import math
-
 class SwerveModule:
     
     def __init__(self) -> None:
