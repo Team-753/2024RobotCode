b83be3456ebfc2e6f2edcb6aad8e214db2f1759b
Sanika <sanikapatterson@icloud.com>
2024-01-24 22:06:36 -0800

    everything should be there

--- subsystems/driveTrain.py
+++ subsystems/driveTrain.py
@@ -1,8 +1,13 @@
-
 import os
 import json
 from networktables import NetworkTables
 import commands2
+import swerveModule
+import wpilib 
+import math
+import navx
+from wpimath import geometry, kinematics
+
 
 class RobotContainer:
     """ Basically does everything. Yeah... """
@@ -20,41 +25,27 @@ class RobotContainer:
 
     def configureButtonBindings(self):
         """ Sets up the button command bindings for the controllers. """
- 13 changes: 7 additions & 6 deletions13  
-subsystems/driveTrain.py
-@@ -1,28 +1,29 @@
-import commands2
-from wpimath import estimator, geometry
-from networktables import NetworkTables
+
 
 class DriveTrainSubsystem(commands2.Subsystem):
-    stateStdDevs = 0.1, 0.1, 0.1
-    visionMeasurementStdDevs = 0.9, 0.9, 0.9 * math.pi
-
 
     def __init__(self, config: dict) -> None:
         super().__init__()
         self.config = config # inhereting the robot config json from the robot container
-        NetworkTables.initialize() # you use networktables to access limelight data
-        poseEstimator = estimator.SwerveDrive4PoseEstimator(self.KINEMATICS, 
-                                                                self.getNAVXRotation2d(), 
-                                                                self.getSwerveModulePositions(), 
-                                                                geometry.Pose2d(0, 0, geometry.Rotation2d(math.pi)), 
-                                                                self.stateStdDevs,
-                                                                self.visionMeasurementStdDevs)
 
-        self.LimelightTable = NetworkTables.getTable('limelight') # giving us access to the limelight's data as a variable
-    def periodic(self) -> None:
-        if self.llTable.getNumber('getpipe', 0) == 0: # 0 being our apriltag pipeline
-            if self.llTable.getNumber('tv', 0) == 1: # are there any valid targets
-        if self.LimelightTable.getNumber('getpipe', 0) == 0: # 0 being our apriltag pipeline
-            if self.LimelightTable.getNumber('tv', 0) == 1: # are there any valid targets
-                if self.alliance == wpilib.DriverStation.Alliance.kBlue:
-                    botPoseData = self.llTable.getNumberArray('botpose_wpiblue', [0,0,0,0,0,0,0])
-                    botPoseData = self.LimelightTable.getNumberArray('botpose_wpiblue', [0,0,0,0,0,0,0])
-                else:
-                    botPoseData = self.llTable.getNumberArray('botpose_wpired', [0,0,0,0,0,0,0])
-                    botPoseData = self.LimelightTable.getNumberArray('botpose_wpired', [0,0,0,0,0,0,0])
-                botPose2D = geometry.Pose2d(geometry.Translation2d(botPoseData[0], botPoseData[1]), geometry.Rotation2d(botPoseData[5]))
-                latency = botPoseData[6]
-                self.poseEstimator.addVisionMeasurement(botPose2D, latency)
+        self.navx = navx.AHRS.create_spi(update_rate_hz=100)
+
+        self.kMaxSpeed = self.config ["RobotDefaultSettings"]["wheelVelocityLimit"]
+        self.kmaxAutoSpeed = self.config ["autonomousSettings"]["autoVelLimit"]
+        self.wheelBase = self.config["RobotDimensions"]["wheelBase"]
+        self.trackWidth = self.config["RobotDimensions"]["trackWidth"]
+
+        self.frontLeft = swerveModule(self.config ["swerveModules"]['frontLeft'], "frontLeft")
+        self.frontRight = swerveModule(self.config ["swerveModules"]['frontRight'], 'frontRight')
+        self.rearLeft = swerveModule(self.config ["serveModules"]['rearLeft'], 'rearLeft')
+        self.rearRight = swerveModule(self.config ["swerveModules"]["rearRight"], "rearRight")
+
+        self.KINEMATICS = kinematics.SwerveDrive2Kinematics( geometry.Translation2d (self.trackWidth / 2, self.wheelBase / 2), geometry.Translation2d (self.trackWidth / 2, -self.wheelBase / 2), geometry.Translation2d(-self.trackWidth / 2, self.wheelBase / 2), geometry.Translation2d (-self.trackWidth / 2, -self.wheelBase /2))
+
+    def joystickDrive (self, joystickX, joystickY, joyStickZ):
+        pass
