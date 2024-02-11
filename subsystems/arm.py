b13af9ae97f66e1141cc981d1558a5c0bcf9dd04
jserra99 <progamer5051@gmail.com>
2024-02-11 12:30:08 -0800

    arm api complete

--- subsystems/arm.py
+++ subsystems/arm.py
@@ -2,41 +2,62 @@
 import wpilib
 import commands2
 import RobotConfig
-from RobotConfig import armConstants
-from RobotConfig import DriveConstants
 import rev
 from rev import _rev
-import wpimath
-from wpimath import kinematics
-"""Needed?^"""
+
 class ArmSubsystem(commands2.Subsystem):
+    kMotorToArmDegrees = 360 / (2.5 * 20) # each rotation of the motor is 7.2 degrees
+    kMotorToArmDegreeVelocity = kMotorToArmDegrees / 60 # each rotation of the motor is 7.2 degrees
     def __init__(self) -> None:
         """ This is ran once, it returns NOTHING """
         super().__init__()
         # Sets motors for arm commands
         self.leftArm = rev.CANSparkMax(RobotConfig.Arm.leftMotorCanID, _rev.CANSparkLowLevel.MotorType.kBrushless)
         self.rightArm = rev.CANSparkMax(RobotConfig.Arm.rightMotorCanID, _rev.CANSparkLowLevel.MotorType.kBrushless)
-        # Sets XBOX controller
-        self.auxControl = wpilib.XboxController(DriveConstants.XBOX.USB_ID)
+        
+        self.leftArm.restoreFactoryDefaults()
+        self.leftArm.setIdleMode(_rev.CANSparkMax.IdleMode.kCoast)
+        self.leftEncoder = self.leftArm.getEncoder()
+        self.leftEncoder.setPositionConversionFactor(self.kMotorToArmDegrees)
+        self.leftEncoder.setVelocityConversionFactor(self.kMotorToArmDegreeVelocity)
+        self.leftPIDController = self.leftArm.getPIDController()
+        self.leftPIDController.setFeedbackDevice(self.leftEncoder)
+        self.leftPIDController.setP(0.1)
+        self.leftPIDController.setI(0)
+        self.leftPIDController.setD(0)
+        self.leftPIDController.setFF(0)
+        self.leftPIDController.setOutputRange(-1,1)
+        self.leftArm.setSmartCurrentLimit(35)
+        self.leftArm.burnFlash()
+        
+        self.rightArm.restoreFactoryDefaults()
+        self.rightArm.setIdleMode(_rev.CANSparkMax.IdleMode.kCoast) # change to brake in final
+        self.rightEncoder = self.rightArm.getEncoder()
+        self.rightEncoder.setInverted(True)
+        self.rightEncoder.setPositionConversionFactor(self.kMotorToArmDegrees)
+        self.rightEncoder.setVelocityConversionFactor(self.kMotorToArmDegreeVelocity)
+        self.rightPIDController = self.rightArm.getPIDController()
+        self.rightPIDController.setFeedbackDevice(self.rightEncoder)
+        self.rightPIDController.setP(0.1)
+        self.rightPIDController.setI(0)
+        self.rightPIDController.setD(0)
+        self.rightPIDController.setFF(0)
+        self.rightPIDController.setOutputRange(-1,1)
+        self.rightArm.setSmartCurrentLimit(35)
+        self.rightArm.burnFlash()
+        
+        self.desiredAngle = 5 # degrees above the floor, this is a arbitrary guess
+        self.leftEncoder.setPosition(self.desiredAngle / self.kMotorToArmDegrees)
+        self.rightEncoder.setPosition(self.desiredAngle / self.kMotorToArmDegrees)
+        
     def periodic(self) -> None:
-        # TODO: Get XBOX control response to set arm. Add this to the "self.rightArm"
-        """Doesn't work
-        self.auxControl.A(function("A"))
-        self.auxControl.B(function("B"))
-        self.auxControl.X(function("X"))
-        self.auxControl.Y(function("Y"))
-        """
-        # Inverts and controls left arm motor based on what right arm is doing.
-        self.rightArm
-        self.leftArm.follow(self.rightArm, True)
-        """ Put code you want to be looped in here. Nothing is returned here either."""
-        return super().periodic()
-    def function(button):
-    # TODO: Add code here
-        # Do something based on button
-        pass
-    def goToSpeaker(self) -> None:
-        # Code that raises arm to speaker height from RobotConfig.py
-        pass
+        # updates the PID controllers to go to the wanted position
+        '''self.leftPIDController.setReference(self.desiredAngle, _rev.CANSparkMax.ControlType.kPosition)
+        self.rightPIDController.setReference(self.desiredAngle, _rev.CANSparkMax.ControlType.kPosition)'''
+        
+        wpilib.SmartDashboard.putNumber("Arm Angle Degrees: ", self.leftEncoder.getPosition())
+    
+    def setDesiredAngle(self, kDesiredAngle: float):
+        self.desiredAngle = kDesiredAngle
 
     
