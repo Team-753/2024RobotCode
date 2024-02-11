608d5df4ed3eea9b9afb8dfff9ce7c0e0d6dc30e
BrendanM9 <124943530+BrendanM9@users.noreply.github.com>
2024-02-11 11:35:00 -0800

    Add files via upload

--- commands/ArmCommands.py
+++ commands/ArmCommands.py
@@ -1,10 +1,20 @@
 import wpilib
 import commands2
 from subsystems.arm import ArmSubsystem
+from subsystems.grabber import grabberSubsystem
 class ArmSpeaker(commands2.Command):
     def __init__(self):
         super().__init__()
         self.arm = ArmSubsystem
     def execute(self) -> None:
         self.arm.goToSpeaker(self)
+class grabberEvents(commands2.Command):
+    def __init__(self):
+        super().__init__()
+        self.grabber = grabberSubsystem    
+    def grab(self) -> None:
+        self.grabber.intake(self)
+    def empty(self) -> None:
+        self.grabber.outtake(self)
+    
         
