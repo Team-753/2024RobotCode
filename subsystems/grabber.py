9525a817562dff81055d7f3ae94a1bad66bdb845
BrendanM9 <124943530+BrendanM9@users.noreply.github.com>
2024-02-18 11:08:55 -0800

    Fixed grabber code

--- subsystems/grabber.py
+++ subsystems/grabber.py
@@ -12,18 +12,16 @@ class grabberSubsystem(commands2.Subsystem):
         # sets motors for grabber. For CAN IDs, use RobotConfig
         self.bottomMotor = rev.CANSparkMax(RobotConfig.grabber.bottomMotorCANID, _rev.CANSparkMax.MotorType.kBrushless)
         self.topMotor = rev.CANSparkMax(RobotConfig.grabber.topMotorCANID, _rev.CANSparkMax.MotorType.kBrushless)
-        self.sensor = wpilib.DigitalInput(RobotConfig.grabber.sensorDIOID)
+        self.topMotor.follow(self.bottomMotor, True)
+        #self.sensor = wpilib.DigitalInput(RobotConfig.grabber.sensorDIOID)
     def periodic(self) -> None:
         # TODO: code to get commands from drive station joysticks
         # sets bottom motor position and tells top motor to follow. Top motor IS inverted.
-        self.bottomMotor
-        self.topMotor.follow(self.bottomMotor, True)
         return super().periodic()
     def intake(self) -> None:
         # TODO: Code to intake ring
         self.bottomMotor.set(RobotConfig.grabber.intake)
-        self.topMotor.follow(self.bottomMotor, True)
-        if(self.sensor.get() != False):
+        '''if(self.sensor.get() != False):
             while(self.sensor.get() != False):
                 # Stop inputting
                 self.bottomMotor.set(RobotConfig.grabber.idle)
@@ -31,11 +29,9 @@ class grabberSubsystem(commands2.Subsystem):
         else:
             # Stop inputting
             self.bottomMotor.set(RobotConfig.grabber.idle)
-            self.topMotor.follow(self.bottomMotor, True)
+            self.topMotor.follow(self.bottomMotor, True)'''
     def outtake(self) -> None:
         # TODO: Code to shoot the ring based on command speed
         self.bottomMotor.set(RobotConfig.grabber.outtake)
-        self.topMotor.follow(self.bottomMotor, True)
     def idle(self) -> None:
         self.bottomMotor.set(RobotConfig.grabber.idle)
-        self.topMotor.follow(self.bottomMotor, True)
