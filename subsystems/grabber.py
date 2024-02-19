cb4d974ac2a61a9073d322c3f6908e548aaa9d1e
BrendanM9 <124943530+BrendanM9@users.noreply.github.com>
2024-02-19 13:54:49 -0800

    Update grabber.py

--- subsystems/grabber.py
+++ subsystems/grabber.py
@@ -14,10 +14,18 @@ class grabberSubsystem(commands2.Subsystem):
         self.topMotor = _rev.CANSparkMax(RobotConfig.grabber.topMotorCANID, _rev.CANSparkMax.MotorType.kBrushless)
         self.topMotor.follow(self.bottomMotor, True)
         #self.sensor = wpilib.DigitalInput(RobotConfig.grabber.sensorDIOID)
+        global released
+        released = True
     def periodic(self) -> None:
         # TODO: code to get commands from drive station joysticks
         # sets bottom motor position and tells top motor to follow. Top motor IS inverted.
         return super().periodic()
+    def releasedVar(self) -> None:
+        released = False
+        return released
+    def unreleasedVar(self) -> None:
+        released = True
+        return released
     def intake(self) -> None:
         # TODO: Code to intake ring
         self.bottomMotor.set(RobotConfig.grabber.intake)
@@ -30,8 +38,18 @@ class grabberSubsystem(commands2.Subsystem):
             # Stop inputting
             self.bottomMotor.set(RobotConfig.grabber.idle)
             self.topMotor.follow(self.bottomMotor, True)'''
-    def outtake(self) -> None:
+        if(self.sensor.get() == False):
+            # Wait until false or released
+            while((self.sensor.get() == True) or (released == True)):
+                self.bottomMotor.set(RobotConfig.grabber.idle)
+        else:
+            # Stop inputting
+            self.bottomMotor.set(RobotConfig.grabber.idle)
+    def outtakeSlow(self) -> None:
         # TODO: Code to shoot the ring based on command speed
-        self.bottomMotor.set(RobotConfig.grabber.outtake)
+        self.bottomMotor.set(RobotConfig.grabber.outtakeS)
+    def outtakeFast(self) -> None:
+        # TODO: Code to shoot the ring based on command speed
+        self.bottomMotor.set(RobotConfig.grabber.outtakeF)
     def idle(self) -> None:
         self.bottomMotor.set(RobotConfig.grabber.idle)
