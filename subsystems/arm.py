79518b3aee395f69467f1393c979411944ca39eb
Joe <73498493+jserra99@users.noreply.github.com>
2024-02-21 22:36:16 -0800

    Commented out robot-breaking arm code, see changes for clarification.

--- subsystems/arm.py
+++ subsystems/arm.py
@@ -52,8 +52,9 @@ class ArmSubsystem(commands2.Subsystem):
         
     def periodic(self) -> None:
         # updates the PID controllers to go to the wanted position
-        self.leftPIDController.setReference(self.desiredAngle, _rev.CANSparkMax.ControlType.kPosition)
-        self.rightPIDController.setReference(self.desiredAngle, _rev.CANSparkMax.ControlType.kPosition)
+        # Leave these lines commented out until you can test if a positive encoder angle change corresponds to the arm lifting up, as is this code being ran risks breaking the arm or worse.
+        # self.leftPIDController.setReference(self.desiredAngle, _rev.CANSparkMax.ControlType.kPosition)
+        # self.rightPIDController.setReference(self.desiredAngle, _rev.CANSparkMax.ControlType.kPosition)
         
         wpilib.SmartDashboard.putNumber("Left Motor Angle Degrees: ", self.leftEncoder.getPosition())
         wpilib.SmartDashboard.putNumber("Right Motor Angle Degrees: ", self.rightEncoder.getPosition())
