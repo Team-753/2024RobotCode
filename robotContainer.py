af65d4347cf0e72e21dfd0edbc8a5561722d9b50
RyanTrissel <87545303+MyBeanman@users.noreply.github.com>
2024-03-01 18:13:21 -0800

    Add files via upload

--- robotContainer.py
+++ robotContainer.py
@@ -95,7 +95,10 @@ class RobotContainer:
         
         """ Logic for what will run in autonomous mode. Returning anything but a command will result in nothing happening in autonomous. """
         pathName = self.autonomousChooser.getSelected()
-        return PathPlannerAuto(pathName)
+        if pathName == "Only Score Note": 
+            commands2.command()
+        else:
+            return PathPlannerAuto(pathName)
     #-----------------------------------------------------------------------------------------------   
     def disabledInit(self):
         pass
